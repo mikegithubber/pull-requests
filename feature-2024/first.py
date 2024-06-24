592ed8661452bbd2276e06defed4e84e3cca5619
Vadim Li <nog453010@gmail.com>
2024-06-24 20:01:19 +0500

    new data-structure

--- feature-2024/first.py
+++ feature-2024/first.py
@@ -1,3 +1,15 @@
-if __name__ == '__main__':
-    print('hello world')
+class Counter:
+    def __init__(self, limit=100) -> None:
+        self.counter = 0
+        self.limit = limit
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        self.counter += 1
+        if self.counter <= self.limit:
+            return self.counter
+        raise StopIteration
+        
     
